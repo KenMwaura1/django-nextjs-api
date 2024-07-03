c996957eefd41750128a50882ead9536ecf396c7
KenMwaura1 <kemwaura@gmail.com>
2024-07-04 02:16:45 +0300

    fixed models

--- src/waitlists/models.py
+++ src/waitlists/models.py
@@ -4,7 +4,8 @@ from django.db import models
 # Create your models here.
 class WaitlistEntry(models.Model):
     email = models.EmailField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
