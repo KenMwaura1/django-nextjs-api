f7dfe1e9f0d9db9646f96b077dec5480eb165cd9
KenMwaura1 <kemwaura@gmail.com>
2024-07-04 01:34:44 +0300

    Changed the schema

--- src/waitlists/schemas.py
+++ src/waitlists/schemas.py
@@ -1,11 +1,19 @@
 from ninja import Schema
 from datetime import datetime
 from pydantic import EmailStr
+
+
 class WaitlistEntryCreateSchema(Schema):
     #Create -> Data
     # WaitlistEntryIn
     email: EmailStr
     name: str
+
+class WaitlistEntryListSchema(Schema):
+    # List -> Data
+    # WaitlistEntryOut
+    id: int
+    email: EmailStr
 
 class WaitlistEntryDetailSchema(Schema):
     # Get  -> Data
