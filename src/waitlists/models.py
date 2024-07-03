d06376101ff26453a967609958308a6649623c4b
KenMwaura1 <kemwaura@gmail.com>
2024-07-04 01:34:28 +0300

    updated the models.py

--- src/waitlists/models.py
+++ src/waitlists/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Waitlist(models.Model):
+class WaitlistEntry(models.Model):
     email = models.EmailField()
     created_at = models.DateTimeField(auto_now_add=True)
 
