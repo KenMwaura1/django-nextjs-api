1da014871451f481ecc927005b53d088d4a78faf
KenMwaura1 <kemwaura@gmail.com>
2024-07-03 09:34:09 +0300

    Added authentication

--- src/backend_api/api.py
+++ src/backend_api/api.py
@@ -1,14 +1,25 @@
 from ninja import NinjaAPI, Schema
 from rich import print
 
-api = NinjaAPI()
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
 
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
 
 class UserSchema(Schema):
     username: str
     is_authenticated: bool
+    email: str = None
 
 
 @api.get("/hello")
+def user(request):
+    print(request)
+    return "Hello World"
+
+
+@api.get("/user", response=UserSchema, auth=JWTAuth())
 def user(request):
     return request.user
